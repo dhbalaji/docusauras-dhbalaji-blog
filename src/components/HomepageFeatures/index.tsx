0a213c65fe2e070791003310fe8671f38c9205e1
dhbalaji <pessebalu@gmail.com>
2022-06-26 10:40:03 +0530

    Hello World MFHB

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.css";
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className={styles.row}>
+          <h1>Minimalist Frontend Hand Book (MFHB)</h1>
+          <h2>under construction...</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
