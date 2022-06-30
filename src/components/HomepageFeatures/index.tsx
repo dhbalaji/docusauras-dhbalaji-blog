f632d889b186833d6cbbf8fc6802e20f64efcaba
dhbalaji <pessebalu@gmail.com>
2022-06-30 09:37:33 +0530

    Home page mod

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -7,6 +7,9 @@ export default function HomepageFeatures(): JSX.Element {
         <div className={styles.row}>
           <h1>Frontend Revision Book (FRB)</h1>
           <h2>under development...</h2>
+          <h3>
+            <a href="/tools-tech/2022">Tech for application developers</a>
+          </h3>
         </div>
       </div>
     </section>
