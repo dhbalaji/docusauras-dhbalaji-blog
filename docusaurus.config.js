59bb371b1b181fcdc628d2474ce0602226b9e843
dhbalaji <pessebalu@gmail.com>
2022-07-01 08:54:23 +0530

    changed repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "dhbalaji software",
   tagline: "Sponsored by dhbalaji.dev",
   url: "https://dhbalaji.github.io",
-  baseUrl: "/frontend-revision-handbook/",
+  baseUrl: "/docusauras-dhbalaji-blog/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
