2c640ff2fef20f92c3c8625baf57a7b91aff2829
dhbalaji <pessebalu@gmail.com>
2023-05-18 11:26:10 +0530

    Renamed hire me to contact me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import "prismjs/themes/prism.css";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
-import styles from "./index.module.css";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
